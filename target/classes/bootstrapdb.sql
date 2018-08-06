ecb47e1e39819c1b798c3a8816317da4ea93bd7b
Milan Timotijevic <mtimotijevic@novomaticls.com>
2018-08-06 17:38:57 +0200

    added sql script to be executed after deploying

---
+++ target/classes/bootstrapdb.sql
@@ -0,0 +1,5 @@
+INSERT into category set text="animals";
+INSERT into category set text="lifestyle";
+INSERT into category set text="cars";
+INSERT into category set text="gaming";
+INSERT into category set text="movies";
